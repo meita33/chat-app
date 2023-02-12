e3da1e524ec353d367c1c64d75b204a51bc3f537
meita33 <124659672+meita33@users.noreply.github.com>
2023-02-12 21:49:43 +0900

    自己修正１

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,9 +4,9 @@ class MessagesController < ApplicationController
      @message = Message.new
      @room = Room.find(params[:room_id])
      @messages = @room.messages.includes(:user)
-   end
+  end
  
-   def create
+  def create
      @room = Room.find(params[:room_id])
      @message = @room.messages.new(message_params)
      if @message.save
@@ -15,12 +15,11 @@ class MessagesController < ApplicationController
        @messages = @room.messages.includes(:user)
        render :index
      end
-   end
+  end
  
-   private
+  private
  
-   def message_params
+  def message_params
      params.require(:message).permit(:content).merge(user_id: current_user.id)
-   end
- end
- 
+  end
+end
